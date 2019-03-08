c7539e94ccdfef7e2f01e3e658425d20d4b90bfb
rony2627 <48362829+rony2627@users.noreply.github.com>
2019-03-09 00:34:38 +0500

    Update minimap.user.js

--- minimap.user.js
+++ minimap.user.js
@@ -18,7 +18,7 @@ Number.prototype.between = function(a, b) {
   return this > min && this < max;
 };
 var range = 25;
-window.baseTepmlateUrl = 'https://raw.githubusercontent.com/MelodyJoy66/PJMAP/master/';
+window.baseTepmlateUrl = 'https://raw.githubusercontent.com/rony2627/.../master/master/';
 
 window.addEventListener('load', function () {
     //Regular Expression to get coordinates out of URL
